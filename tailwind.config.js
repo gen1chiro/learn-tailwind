0b77b8529accbeec856fcdce901202d939d583aa
gen1chiro <juljavellana1218@gmail.com>
2024-12-30 18:38:40 +0800

    add pricingTable

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/index.html", "./src/pomodoro.html", "./src/testimonial.html"],
+  content: ["./src/index.html",
+            "./src/pomodoro.html",
+            "./src/testimonial.html",
+            "./src/pricingTable.html"],
   theme: {
     extend: {},
   },
