b114d61265ebbeaaa12cd4a1466735c95e71fc98
gen1chiro <juljavellana1218@gmail.com>
2024-12-31 19:48:34 +0800

    add preferencesForm

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/index.html",
+  content: [
+            "./src/index.html",
             "./src/pomodoro.html",
             "./src/testimonial.html",
-            "./src/pricingTable.html"],
+            "./src/pricingTable.html",
+            "./src/preferencesForm.html"
+            ],
   theme: {
     extend: {},
   },
