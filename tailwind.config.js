7e20876cbd481dc73774140c04c3d26c795f3277
gen1chiro <juljavellana1218@gmail.com>
2025-01-03 11:54:10 +0800

    update file structure & file paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-            "./src/index.html",
-            "./src/pomodoro.html",
-            "./src/testimonial.html",
-            "./src/pricingTable.html",
-            "./src/preferencesForm.html",
-            "./src/404Page.html"
+            "./index.html",
+            "./pomodoro.html",
+            "./testimonial.html",
+            "./pricingTable.html",
+            "./preferencesForm.html",
+            "./404Page.html"
             ],
   theme: {
     extend: {},
